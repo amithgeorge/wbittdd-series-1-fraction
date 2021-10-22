7768c9fca1acbab760fc465b0e38c3186083e18c
Amith George <strider.am@gmail.com>
2021-10-22 21:18:44 +0530

    fractions add
    - can add whole numbers to each other
    - can add wholes numbers to fractions
    - can add fractions to each other, same denominator or different
    - can handle negative whole numbers, or negative numerators

--- src/amithgeorge/fractions.clj
+++ src/amithgeorge/fractions.clj
@@ -1,15 +1,54 @@
-(ns amithgeorge.fractions
-  (:gen-class))
+(ns amithgeorge.fractions)
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+(defn- gcd
+  [a b]
+  (let [remainder (mod a b)]
+    (if (pos? remainder)
+      (recur b remainder)
+      b)))
 
 (comment
-  (greet {:name "Amith"}))
+  (= 3 (gcd 6 9))
+  (= 3 (gcd 9 6))
+  (= 1 (gcd 7 5)))
+
+(defn ->fraction
+  [numerator denominator]
+  {:numerator numerator
+   :denominator denominator})
+
+(defn- reduce-fraction [result]
+  (let [result-gcf (gcd (:numerator result) (:denominator result))]
+    (if (= 1 result-gcf)
+      result
+      (->fraction (/ (:numerator result) result-gcf)
+                  (/ (:denominator result) result-gcf)))))
+
+(defn fraction-is-whole? [reduced-fraction]
+  (= 1 (:denominator reduced-fraction)))
+
+(defn add
+  [x y]
+  (let [x (if (int? x) (->fraction x 1) x)
+        y (if (int? y) (->fraction y 1) y)]
+    (if (and (map? x)
+             (map? y))
+      (let [result (if (= (:denominator x) (:denominator y))
+                     (->fraction (+ (:numerator x) (:numerator y))
+                                 (:denominator x))
+                     (let [denominator-lcm (/ (* (:denominator x)
+                                                 (:denominator y))
+                                              (gcd (:denominator x) (:denominator y)))
+                           x-multiplier (/ denominator-lcm (:denominator x))
+                           y-muliplier (/ denominator-lcm (:denominator y))
+                           x' (->fraction (* x-multiplier (:numerator x))
+                                          (* x-multiplier (:denominator x)))
+                           y' (->fraction (* y-muliplier (:numerator y))
+                                          (* y-muliplier (:denominator y)))]
+                       (->fraction (+ (:numerator x') (:numerator y'))
+                                   (:denominator x'))))
+            reduced-fraction (reduce-fraction result)]
+        (if (fraction-is-whole? reduced-fraction)
+          (:numerator reduced-fraction)
+          reduced-fraction))
+      (+ x y))))
--- test/amithgeorge/fractions_test.clj
+++ test/amithgeorge/fractions_test.clj
@@ -1,5 +1,73 @@
 (ns amithgeorge.fractions-test
-  (:require [clojure.test :refer :all]
-            [amithgeorge.fractions :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [amithgeorge.fractions :as sut]))
+
+(deftest add-fractions
+  (testing "adding 0 to whole numbers"
+    (is (= 0 (sut/add 0 0)))
+    (is (= 1 (sut/add 0 1)))
+    (is (= 2 (sut/add 2 0)))
+    (is (= -1 (sut/add -1 0)))
+    (is (= -2 (sut/add 0 -2))))
+
+  (testing "adding whole numbers"
+    (is (= 9 (sut/add 4 5)))
+    (is (= -7 (sut/add -3 -4)))
+    (is (= -5 (sut/add -7 2))))
+
+  (testing "adding fractions with same denominator"
+    (is (= (sut/->fraction 3 5)
+           (sut/add (sut/->fraction 1 5)
+                    (sut/->fraction 2 5))))
+
+    (is (= (sut/->fraction -2 5)
+           (sut/add (sut/->fraction -3 5)
+                    (sut/->fraction 1 5))))
+
+    (testing "resultant fraction should be in reduced form"
+      (is (= (sut/->fraction 1 2)
+             (sut/add (sut/->fraction 3 8)
+                      (sut/->fraction 1 8))))
+      (is (= (sut/->fraction 1 2)
+             (sut/add (sut/->fraction -1 4)
+                      (sut/->fraction 3 4))))
+      (is (= (sut/->fraction 1 2)
+             (sut/add (sut/->fraction 3 4)
+                      (sut/->fraction -1 4)))))
+
+    (testing "return whole number if resultant denominator is 1"
+      (is (= 1
+             (sut/add (sut/->fraction 1 3)
+                      (sut/->fraction 2 3))))))
+
+  (testing "adding fractions with different denominators"
+    (is (= (sut/->fraction 11 8)
+           (sut/add (sut/->fraction 3 4)
+                    (sut/->fraction 5 8))))
+
+    (is (= (sut/->fraction 11 18)
+           (sut/add (sut/->fraction 1 6)
+                    (sut/->fraction 4 9))))
+
+    (is (= (sut/->fraction -11 15)
+           (sut/add (sut/->fraction -4 5)
+                    (sut/->fraction 1 15)))))
+
+  (testing "adding fraction and whole number"
+    (is (= (sut/->fraction 3 2)
+           (sut/add 1
+                    (sut/->fraction 1 2))))
+
+    (is (= (sut/->fraction 23 5)
+           (sut/add (sut/->fraction 3 5)
+                    4)))
+
+    (is (= (sut/->fraction -29 9)
+           (sut/add (sut/->fraction 7 9)
+                    -4)))
+
+    (is (= (sut/->fraction 1 3)
+           (sut/add (sut/->fraction -2 3)
+                    1)))))
 
 
